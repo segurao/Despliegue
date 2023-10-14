f0cd5861b2c0e1982aaa5f486a48b1d4efdc7e1f
segurao <g.segurao@uniandes.edu.co>
2023-10-14 09:17:29 -0500

    Actualizacion host

--- app.py
+++ app.py
@@ -244,6 +244,5 @@ def update_output_div(date, hour, proy):
 
 # Run the server
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(host="0.0.0.0", port=8090, debug=True)
     
-ctrl + c
